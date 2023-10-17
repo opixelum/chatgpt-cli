2e30d9b307ac0305ec0e2ad38a2c37776d4c1e34
Opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-17 20:49:01 +0200

    Define chat function (#7)

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 
+def chat_with_gpt(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content.strip()
+
+
 def main():
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
