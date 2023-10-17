68027e90c3e2774c2249771f383622bab1255a3c
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-17 20:40:50 +0200

    Define OpenAI API key

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 def main():
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
-    print(os.getenv("OPENAI_API_KEY"))
+
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
 if __name__ == '__main__':
