ff144ac5d3847adb1f281e9a0e62dc5d537e7fe5
Opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-17 20:42:35 +0200

    Add OpenAI AI key from `.env` (#5)
    * Refactor `README.md`
    
    * Import dotenv & get OpenAI API key from `.env` file
    
    * Define OpenAI API key

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import openai
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+def main():
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+
+if __name__ == '__main__':
+    main()
