a01b077059bee482ca95e1213d25e42e83852375
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-18 08:54:59 +0200

    Add comments

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ from utils import clear_line
 
 
 def main():
+    # Load OpenAI API key from .env file
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     api_key = os.environ.get("OPENAI_API_KEY")
     chatgpt.init(api_key)
 
+    # Header and instructions
     print("Welcome to ChatGPT CLI! Type 'exit', 'quit' or 'bye' to quit.\n")
 
     while True:
+        # Get user input
         prompt = input("You: ")
-
         if prompt.lower() in ["exit", "quit", "bye"]:
             break
 
+        # Print response
         print("\nThinking...")
         response = chatgpt.chat(prompt)
         clear_line()
