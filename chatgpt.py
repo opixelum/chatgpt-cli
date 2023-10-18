d71965aee18e0ac9cc9e18a5e74308b36f0fb4d6
Opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-18 10:16:21 +0200

    Catch OpenAI API rate limit error (#21)

--- chatgpt.py
+++ chatgpt.py
@@ -1,4 +1,5 @@
 import openai
+from colors import Colors
 
 
 def init(api_key):
@@ -6,8 +7,15 @@ def init(api_key):
 
 
 def chat(prompt):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}]
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}]
+        )
+    except openai.error.RateLimitError:
+        return Colors.RED + "Looks like you've hit the API rate limit, or "\
+            "your account is out of credits. Check "\
+            "https://platform.openai.com/account/billing and try again later."\
+            + Colors.RESET
+
     return response.choices[0].message.content.strip()
